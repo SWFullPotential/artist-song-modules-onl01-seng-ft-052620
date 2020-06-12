085be6eb84f7d9e3c5049afe2548f51dc91b6db3
Samantha Wabi <swfullpotential@gmail.com>
2020-06-12 01:49:15 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,8 +1,8 @@
 module Findable
   
   def ClassMethods
-    def find_by_name
-      @@all.detect{|a| a.name == name}
+    def find_by_name(name)
+      return self.all.detect{|a| a.name == name}
     end
   end
   
